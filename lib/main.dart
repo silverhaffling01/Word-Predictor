5169824b2da53c5a916ea163e71dc9c8c46daf2d
Anish Deshpande <anishmdeshpande@gmail.com>
2023-11-05 23:56:18 +0530

    Login/Signup pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:inheritance/pages/homepage.dart';
+import 'package:inheritance/pages/loginpage.dart';
+import 'package:inheritance/pages/signup.dart';
 
 void main() => runApp(MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/signup': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
 
+
+
+      },
   )
 );
